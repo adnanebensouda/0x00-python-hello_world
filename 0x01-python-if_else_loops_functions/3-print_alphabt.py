ce385d522f70126c979e7780cb53a817408c11b6
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-06-06 14:38:43 -0700

    i just finish the task 4

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for alphabt in range(97, 123):
+        if chr(alphabt) == 'q' or chr(alphabt) == 'e':
+                    continue
+                    print(chr(alphabt).format(alphabt), end='')
