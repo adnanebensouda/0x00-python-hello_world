7725a8a47444982af1a5e1972f4f7beb0bc97770
ADNANE BEN SOUDA <76470837+adnanebensouda@users.noreply.github.com>
2023-06-27 22:17:00 +0100

    task 1 it's done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defination a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize it's new Square.
+
+        Args:
+            and size (int): it's The size of the new square.
+        """
+        self.__size = size
