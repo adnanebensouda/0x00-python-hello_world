73cceff95fbc7cf9167b59a590ae326e5c059fca
ADNANE BEN SOUDA <76470837+adnanebensouda@users.noreply.github.com>
2023-06-28 16:32:07 +0100

    task 12 roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    decs = [roman_dictionary[x] for c in roman_string]
+    output = 0
+    for b in range(len(decs)):
+        output += decs[b]
+        if decs[b - 1] < decs[b] and b != 0:
+            output -= (decs[b - 1] + decs[b - 1])
+    return output
