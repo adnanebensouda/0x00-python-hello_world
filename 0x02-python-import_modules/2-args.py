0e31567ff29dd5d87fb51b45d85320c351e63c2e
ADNANE BEN SOUDA <76470837+adnanebensouda@users.noreply.github.com>
2023-06-28 19:18:01 +0100

    task 2 args done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-counte = len(sys.argv) - 1
-if counte == 0:
-    print("0 arguments.")
-elif counte == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(counte))
-for j in range(counte):
-     print("{}: {}".format(j + 1, sys.argv[j + 1]))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for j in range(count):
+        print("{}: {}".format(j + 1, sys.argv[j + 1]))
