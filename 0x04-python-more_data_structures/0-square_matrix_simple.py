ba13693ab8a725b96fe21910b66273afbc562200
ADNANE BEN SOUDA <76470837+adnanebensouda@users.noreply.github.com>
2023-06-28 15:47:09 +0100

    fix it

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map((lambda p: p * p), elm)) for elim in matrix]
+    return [list(map((lambda p: p * p), elim)) for elim in matrix]
