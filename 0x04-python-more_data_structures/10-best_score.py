1d2bb43464c9e998b9c81556c05740f17cdcd84a
ADNANE BEN SOUDA <76470837+adnanebensouda@users.noreply.github.com>
2023-06-28 16:27:40 +0100

    10 best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary and len(a_dictionaryt):
-        max = list(a_dictionary.keys())[0]
-        for key in a_dictionary:
-            if a_dictionary[key] > a_dictionary[max]:
-                max = key
-        return max
-    return None
+    if not a_dictionary:
+        return (None)
+    return (max(a_dictionary, key=a_dictionary.get))
