3cd2c38bee0e89a51398f785b65f8c78e66263c8
ADNANE BEN SOUDA <76470837+adnanebensouda@users.noreply.github.com>
2023-06-29 20:08:08 +0100

    task 3 infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+
+if __name__ == "__main__":
+    """Printing the addition of all arguments."""
+    import sys
+
+    total = 0
+    for b in range(len(sys.argv) - 1):
+        total += int(sys.argv[b + 1])
+    print("{}".format(total))
