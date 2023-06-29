cb8c65d2829d182213f11c40bd2c5a1139f9f001
ADNANE BEN SOUDA <76470837+adnanebensouda@users.noreply.github.com>
2023-06-29 22:00:37 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print all names defineding by hidden_4 module."""
     import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    for c in dir(hidden_4):
+        if c[:2] != "__":
+            print("{:c}".format(c))
