4f66c3edf309c3bb28297f1e0829f9dd3445b54e
ADNANE BEN SOUDA <76470837+adnanebensouda@users.noreply.github.com>
2023-07-31 17:15:51 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Defines a name-print function."""
+"""Defines a name-printing function."""
 
 
 def say_my_name(first_name, last_name=""):
     """Print a name.
 
     Args:
-        first_name (str): first name to print.
-        last_name (str): last name to print.
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
     Raises:
         TypeError: If either of first_name or last_name are not strings.
     """
