0cae4a67d790308708038230373936b0cbf575d5
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-10-24 23:27:53 +0100

    i delete it

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Define object attribute lookup function."""
-
-
-def lookup(obj):
-    """Return a list of an object available attribute."""
-    return (dir(obj))
--- 0x0B-python-input_output/3-to_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Define string JSON function."""
-import json
-
-
-def to_json_string(my_obj):
-    """Return JSON representation of a string object."""
-    return json.dumps(my_obj)
