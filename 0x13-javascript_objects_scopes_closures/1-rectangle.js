191fa5cfe5ca8d5bb624618d7eb7fec813d3253b
adnanenet <bensouda.adnane.dev@gmail.com>
2024-03-30 07:56:06 -0700

    done

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-"""rectangle class done"""
 class Rectangle {
 	  constructor (w, h) {
 		      this.width = w;
 		      this.height = h;
 		    }
 }
-
 module.exports = Rectangle;
