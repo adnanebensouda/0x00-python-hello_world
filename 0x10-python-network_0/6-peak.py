f5118836bbac63f177eb23069b3c2af909a3c62b
cyberjujutsu <bensouda.adnane.dev@gmail.com>
2024-04-01 16:53:59 +0000

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,28 +4,28 @@
 
 
 def find_peak(list_of_integers):
-        """
-            Args:
-                    list_of_integers(int): list of integers to find peak of
-                        Returns: peak of list_of_integers or None
-                            """
-                                size = len(list_of_integers)
-                                    mid_e = size
-                                        mid = size // 2
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or none
+    """
+    size = len(list_of_integers)
+    mid_e = sizes
+    mid = sizes // 2
 
-                                            if size == 0:
-                                                        return None
+    if sizes == 0:
+        return None
 
-                                                        while True:
-                                                                    mid_e = mid_e // 2
-                                                                            if (mid < size - 1 and
-                                                                                                    list_of_integers[mid] < list_of_integers[mid + 1]):
-                                                                                            if mid_e // 2 == 0:
-                                                                                                                mid_e = 2
-                                                                                                                            mid = mid + mid_e // 2
-                                                                                                                                    elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-                                                                                                                                                    if mid_e // 2 == 0:
-                                                                                                                                                                        mid_e = 2
-                                                                                                                                                                                    mid = mid - mid_e // 2
-                                                                                                                                                                                            else:
- 
+    while True:
+        mid_e = mid_e // 2
+        if (mid < sizes - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
+        else:
+            return list_of_integers[mid]
